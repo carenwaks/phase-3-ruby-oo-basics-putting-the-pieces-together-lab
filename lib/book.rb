7e6de2d3c1db314b23c444096eb1555804cc3c0f
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-05-18 00:11:54 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page
 
+    def initialize(title)
+        @title = title
+    end
 
+    def properties
+        @title = title
+        @author = author_name
+        @page_count = page_count
+        @genre = genre
+    end
+    
+    def turn_page
+        @turn_page = puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    attr_accessor :turn_the_page
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def properties
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+    
+    def cobble
+        @cobble = puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
